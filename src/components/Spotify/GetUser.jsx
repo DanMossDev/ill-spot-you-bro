3231ca846d6c8e10d331cf6d31d65918a85ccdd5
Moss <danielsmoss96@gmail.com>
2022-07-26 21:40:34 +0100

    Fix redirect, rename app

--- src/components/Spotify/GetUser.jsx
+++ src/components/Spotify/GetUser.jsx
@@ -6,6 +6,7 @@ function spotifyAuth() {
     const authEndpoint = 'https://accounts.spotify.com/authorize'
     const clientID = 'd5cf965142af4914a39b012b6e6dbbcf'
     const redirectURI = window.location.href
+    console.log(redirectURI)
     const scopes = ['playlist-read-private', 'playlist-read-collaborative'].join('%20')
 
     window.location = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scopes}&response_type=token&show_dialog=true`
